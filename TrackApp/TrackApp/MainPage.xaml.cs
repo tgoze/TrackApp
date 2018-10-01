b6a52386271ff03f099e8d77b337eac51ca8d98a
TJ Gozé <tjgoze@gmail.com>
2018-10-01 18:48:58 -0500

    Redesigned Main paige to separate XAML

--- TrackApp/TrackApp/MainPage.xaml.cs
+++ TrackApp/TrackApp/MainPage.xaml.cs
@@ -15,58 +15,12 @@ namespace TrackApp
     {
         public MainPage ()
         {
-            this.Title = "HomePage";
-
-            Xamarin.Forms.Button beepButton = new Xamarin.Forms.Button
-            {
-                Text = "Click beep",
-                VerticalOptions = LayoutOptions.CenterAndExpand,
-                HorizontalOptions = LayoutOptions.Center                
-            };
-            beepButton.Clicked += BeepButton_Clicked;
-
-            this.Children.Add(new ContentPage
-            {
-                Title = "Run",
-                Content = new StackLayout
-                {
-                    HeightRequest = 100f,
-                    VerticalOptions = LayoutOptions.Center,                    
-                    Children =
-                    {
-                        beepButton       
-                    }
-                }                          
-            }
-            );
-
-            this.Children.Add(new ContentPage
-            {
-                Title = "History",
-                Content = new StackLayout
-                {                                    
-                }
-            }
-            );
-
-            this.Children.Add(new ContentPage
-            {
-                Title = "Settings",
-                Content = new StackLayout
-                {
-                }
-            }
-            );
+            this.Title = "HomePage";                        
 
             InitializeComponent();
 
             // Sets the tabs to the bottom of the screen
             On<Xamarin.Forms.PlatformConfiguration.Android>().SetToolbarPlacement(ToolbarPlacement.Bottom);
-        }
-
-        private void BeepButton_Clicked(object sender, EventArgs e)
-        {
-            DependencyService.Get<IBeep>().playBeep();
         }
     }            
 }
---
+++ TrackApp/TrackApp/Run.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrackApp
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Run : ContentPage
+	{
+		public Run ()
+		{
+            InitializeComponent ();
+		}
+
+        private void BeepButton_Clicked(object sender, EventArgs e)
+        {
+            DependencyService.Get<IBeep>().playBeep();
+        }
+    }
+}
