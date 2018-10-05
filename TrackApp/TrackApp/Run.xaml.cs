4346648a8c276a25f8c46a67c09e36c8fdbb89f9
Dakota Dalton <1silvertiger@gmail.com>
2018-10-05 12:13:57 -0500

    Metronome
    The metronome works for thirty seconds...I don't know why it stops working after that.
    Split functionality is on the display. It should be trivial to use objects to keep track of this.

--- TrackApp/TrackApp/Run.xaml.cs
+++ TrackApp/TrackApp/Run.xaml.cs
@@ -1,21 +1,54 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace TrackApp
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class Run : ContentPage
-	{
-		public Run ()
-		{
-            InitializeComponent ();
-		}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Run : ContentPage
+    {
+        private bool continueTimer = false;
+
+        private int totalCount = 0;
+        private int splitCount = 0;
+
+        public Run()
+        {
+            InitializeComponent();
+        }
+
+        protected void StartBeeper(int timeInterval)
+        {
+            Device.StartTimer(TimeSpan.FromSeconds(timeInterval), () =>
+            {
+                SplitLbl.Text = (splitCount += timeInterval).ToString();
+                TotalLbl.Text = (totalCount += timeInterval).ToString();
+
+                DependencyService.Get<IAudio>().PlayAudioFile("button.mp3");
+
+                return continueTimer;
+            });
+        }
+
+        private void StartBtn_Clicked(object sender, EventArgs e)
+        {
+            if (StartBtn.Text.Equals("Start"))
+            {
+                StartBtn.Text = "Split";
+                StopBtn.IsEnabled = true;
+                StartBeeper(2);
+                continueTimer = true;
+            } else
+            {
+                splitCount = 0;
+            }
+        }
+
+        private void StopBtn_Clicked(object sender, EventArgs e)
+        {
+            continueTimer = false;
+        }
 
         private void BeepButton_Clicked(object sender, EventArgs e)
         {
