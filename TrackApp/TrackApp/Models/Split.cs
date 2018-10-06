e0d2030916e44893feff11998fa1e3641293245f
TJ Gozé <tjgoze@gmail.com>
2018-10-06 16:34:48 -0500

    Organized Structure / Model
    Organized the code structure to follow MVVM. Added some basic classes in the model and used data binding layer for event handling.

---
+++ TrackApp/TrackApp/Models/Split.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrackApp.Models
+{
+    class Split
+    {
+        public int SplitId { get; set; }
+        public int RunId { get; set; }
+        public TimeSpan SplitTime { get; set; }
+    }
+}
---
+++ TrackApp/TrackApp/ViewModels/RunViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace TrackApp.ViewModels
+{
+    public class RunViewModel
+    {
+        public RunViewModel()
+        {
+            AudioCommand = new Command(PlayBeep);
+        }
+
+        public Command AudioCommand { get; }
+
+        private void PlayBeep()
+        {
+            DependencyService.Get<IAudio>().PlayAudioFile("button.mp3");
+        }
+    }
+}
