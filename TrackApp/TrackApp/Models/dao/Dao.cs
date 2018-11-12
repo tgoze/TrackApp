8f192fd60a8670e702ea4e13a297215e47d9d14c
Dakota Dalton <1silvertiger@gmail.com>
2018-11-12 11:17:19 -0600

    Adjusted DAO to reflect changes in webservice

--- TrackApp/TrackApp/Models/dao/Dao.cs
+++ TrackApp/TrackApp/Models/dao/Dao.cs
@@ -1,42 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Http;
 using System.Text;
 
 namespace TrackApp.Models.dao
 {
     abstract class Dao
     {
+        protected static HttpClient client1 = new HttpClient();
         protected static WebClient client = new WebClient();
-        protected static string Url = "https://www.offbynone.net:8785";
+        protected static string Url = "http://www.offbynone.net:8785";
         protected static string FindAllRoles = "FindAllRoles";
 
         //\\//\\//\\//USER STRINGS\\//\\//\\//\\
         //Create
-        protected static string CreateUser = "CreateUser/";
+        protected static string CreateUser = "CreateUser";
 
         //Read
-        protected static string GetUserById = "GetUserById/";
-        protected static string GetUserByUsername = "GetUserByUserName/";
+        protected static string GetUserById = "FindUserById";
+        protected static string GetUserByUsername = "FindUserByUserName";
 
         //Update
-        protected static string UpdateUserById = "UpdateUserById/";
+        protected static string UpdateUserById = "UpdateUserById";
 
         //Delete
-        protected static string DeleteUserById = "DeleteUserById/";
+        protected static string DeleteUserById = "DeleteUserById";
 
         //\\//\\//\\//TEAM STRINGS\\//\\//\\//\\
-        protected static string GetTeamsByUserId = "GetTeamsByUserId/";
+        protected static string GetTeamsByUserId = "GetTeamsByUserId";
 
         //\\//\\//\\//GROUP STRINGS\\//\\//\\//\\
-        protected static string GetGroupByUserId = "GetGroupsByUserId/";
+        protected static string GetGroupByUserId = "GetGroupsByUserId";
 
         //\\//\\//\\//ROLE STRINGS\\//\\//\\//\\
-        protected static string GetAllRoles = "GetAllRoles/1";
+        protected static string GetAllRoles = "FindAllRoles";
 
         static Dao()
         {
             client.BaseAddress = Url;
+            client1.BaseAddress = new System.Uri(Url);
         }
     }
 }
