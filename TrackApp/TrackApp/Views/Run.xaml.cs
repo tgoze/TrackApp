dc1f09b706c45aa3be74e6c34ed306a6613c7d50
AndrijaMarkovic <andrija.markovic.mne@gmail.com>
2018-11-28 11:00:09 -0600

    Merged the Run and Group Run view
    Still no functionality

--- TrackApp/TrackApp/Views/Run.xaml.cs
+++ TrackApp/TrackApp/Views/Run.xaml.cs
@@ -42,12 +42,13 @@ namespace TrackApp
 
         private void SetProgressBarColors()
         {
-            RangeColorCollection rangeColors = new RangeColorCollection();
-
-            rangeColors.Add(new RangeColor() { Color = Color.FromHex("88A0D9EF"), IsGradient = true, Start = 0, End = progressBar.Maximum * .25 });
-            rangeColors.Add(new RangeColor() { Color = Color.FromHex("AA62C1E5"), IsGradient = true, Start = progressBar.Maximum * .25, End = progressBar.Maximum * .5 });
-            rangeColors.Add(new RangeColor() { Color = Color.FromHex("DD20A7DB"), IsGradient = true, Start = progressBar.Maximum * .5, End = progressBar.Maximum * .75 });
-            rangeColors.Add(new RangeColor() { Color = Color.FromHex("FF1C96C5"), IsGradient = true, Start = progressBar.Maximum * .75, End = progressBar.Maximum });
+            RangeColorCollection rangeColors = new RangeColorCollection
+            {
+                new RangeColor() { Color = Color.FromHex("88A0D9EF"), IsGradient = true, Start = 0, End = progressBar.Maximum * .25 },
+                new RangeColor() { Color = Color.FromHex("AA62C1E5"), IsGradient = true, Start = progressBar.Maximum * .25, End = progressBar.Maximum * .5 },
+                new RangeColor() { Color = Color.FromHex("DD20A7DB"), IsGradient = true, Start = progressBar.Maximum * .5, End = progressBar.Maximum * .75 },
+                new RangeColor() { Color = Color.FromHex("FF1C96C5"), IsGradient = true, Start = progressBar.Maximum * .75, End = progressBar.Maximum }
+            };
 
             progressBar.RangeColors = rangeColors;
         }
