1dcacf17c8682b8b27a9e17ef75b2ae77572cfda
Dakota Dalton <1silvertiger@gmail.com>
2018-12-03 21:19:20 -0600

    Adjusted Run to just essentials

--- TrackApp/TrackApp/Models/obj/Run.cs
+++ TrackApp/TrackApp/Models/obj/Run.cs
@@ -5,18 +5,13 @@ namespace TrackApp.Models
 {
     class Run
     {
-        public int Id { get; set; }
-        public User User { get; set; }
-        public Event Event { get; set; }
-        public TimeSpan OriginalTime { get; set; }
-        public TimeSpan UpdatedTime { get; set; }
-        public TimeSpan PredictedTime { get; set; }
-        public User OriginalTimeRecordedBy { get; set; }
-        public User UpdatedTimeRecordedBy { get; set; }
+        public Run()
+        {
+            Splits = new List<Split>();
+        }
+
+        public int RunnerNumer { get; set; }
         public List<Split> Splits { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
-        public DateTime DeletedAt { get; set; }
 
         public TimeSpan TotalTime
         {
