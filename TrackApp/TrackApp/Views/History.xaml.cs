105ec1b3993da292e06bb31bfbaf4a6f079fca4e
TJ Gozé <tjgoze@gmail.com>
2018-12-18 22:24:44 -0600

    Show split index

--- TrackApp/TrackApp/Views/History.xaml.cs
+++ TrackApp/TrackApp/Views/History.xaml.cs
@@ -18,48 +18,5 @@ namespace TrackApp
             InitializeComponent();
             BindingContext = new HistoryViewModel();
         }
-        private ArrayList splitsList = new ArrayList();
-        private List<Models.Run> runs = new List<Models.Run>()
-        {
-             new Models.Run() { RunnerNumber = 1, StrSplits = new List<string>() { "12", "34","34","34","34","66","77","88","99","134" }, StrTotalTime = "4:26" }
-                , new Models.Run() { RunnerNumber = 2, StrSplits = new List<string>() { "56", "78" }, StrTotalTime = "8:49" }
-                , new Models.Run() { RunnerNumber = 3, StrSplits = new List<string>() { "90", "00" }, StrTotalTime = "1:20" }
-        };
-
-        protected override void OnAppearing()
-        {
-            Results.ItemsSource = runs;
-            //createList(runs);
-            //picker.ItemsSource = new List<String>()
-            //{
-            //    "Testing1", "Testing2", "Testing3"
-            //};            
-        }
-
-        private void createList(List<Models.Run> pRuns)
-        {
-            StringBuilder sb = new StringBuilder();
-            foreach(Models.Run run in pRuns)
-            {
-                foreach(string split in run.StrSplits)
-                {
-                    //Console.WriteLine(split);
-                   // Test.Text = split;
-                    
-                }
-                
-                //sb.Append(string.Format("Split: {0} | Value: {1}", properties.Name, properties.GetValue(run, null)));                                    
-            }
-        }
-
-        private void UpdateBtn_Clicked(object sender, EventArgs e)
-        {
-            if (Application.Current.Properties.ContainsKey("1"))
-            {
-                var jsonData = Application.Current.Properties["1"] as string;
-                Models.Run run = JsonConvert.DeserializeObject<Models.Run>(jsonData);
-            }
-        }
-
     }
 }
