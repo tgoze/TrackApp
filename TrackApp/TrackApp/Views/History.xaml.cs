5956f984fb16c9fa0d945d0747eebd999e71771c
Brian <codyhofmann@gmail.com>
2018-12-18 21:55:28 -0600

    Better History Split Display
    Can pick best split

--- TrackApp/TrackApp/Views/History.xaml.cs
+++ TrackApp/TrackApp/Views/History.xaml.cs
@@ -1,8 +1,12 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Reflection;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 using TrackApp.ViewModels;
+using System.Text;
 
 namespace TrackApp
 {
@@ -14,31 +18,47 @@ namespace TrackApp
             InitializeComponent();
             BindingContext = new HistoryViewModel();
         }
+        private ArrayList splitsList = new ArrayList();
+        private List<Models.Run> runs = new List<Models.Run>()
+        {
+             new Models.Run() { RunnerNumber = 1, StrSplits = new List<string>() { "12", "34","34","34","34","66","77","88","99","134" }, StrTotalTime = "4:26" }
+                , new Models.Run() { RunnerNumber = 2, StrSplits = new List<string>() { "56", "78" }, StrTotalTime = "8:49" }
+                , new Models.Run() { RunnerNumber = 3, StrSplits = new List<string>() { "90", "00" }, StrTotalTime = "1:20" }
+        };
 
         protected override void OnAppearing()
         {
-            //Results.ItemsSource = new List<Models.Run>()
-            //{
-            //    new Models.Run() { RunnerNumber = 1, Splits = new List<string>() { "12", "34" }, TotalTime = "4:26" }
-            //    , new Models.Run() { RunnerNumber = 2, Splits = new List<string>() { "56", "78" }, TotalTime = "8:49" }
-            //    , new Models.Run() { RunnerNumber = 3, Splits = new List<string>() { "90", "00" }, TotalTime = "1:20" }
-            //};
+            Results.ItemsSource = runs;
+            //createList(runs);
             //picker.ItemsSource = new List<String>()
             //{
             //    "Testing1", "Testing2", "Testing3"
             //};            
         }
 
-        public void SetRuns(List<Run> runs)
+        private void createList(List<Models.Run> pRuns)
         {
-            //HistoryLv.ItemsSource = runs;
+            StringBuilder sb = new StringBuilder();
+            foreach(Models.Run run in pRuns)
+            {
+                foreach(string split in run.StrSplits)
+                {
+                    //Console.WriteLine(split);
+                   // Test.Text = split;
+                    
+                }
+                
+                //sb.Append(string.Format("Split: {0} | Value: {1}", properties.Name, properties.GetValue(run, null)));                                    
+            }
         }
 
-        private void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        private void UpdateBtn_Clicked(object sender, EventArgs e)
         {
-            Picker picker = sender as Picker;
-            var selectedItem = picker.SelectedItem; // This is the model selected in the picker
-            //Display.Text = "Test";
+            if (Application.Current.Properties.ContainsKey("1"))
+            {
+                var jsonData = Application.Current.Properties["1"] as string;
+                Models.Run run = JsonConvert.DeserializeObject<Models.Run>(jsonData);
+            }
         }
 
     }
